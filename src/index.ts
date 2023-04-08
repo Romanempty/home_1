a9f6632352fec030a0eca675dd6ca95e2088502f
Romanempty <romkilmetov2000@gmail.com>
2023-04-08 16:41:17 +0400

    5comit

--- src/index.ts
+++ src/index.ts
@@ -132,11 +132,6 @@ app.put('/videos/:id', (req: Request, res: Response) => {
                 }
         }
         if (typeof newVideo.publicationDate === 'string') {
-            let r = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|w([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
-            if (!r.test(newVideo.publicationDate)) {
-                errorsArray.push({message: 'error', field: 'publicationDate'})
-            }
-        } else {
             errorsArray.push({message: 'error', field: 'publicationDate'})
         }
         if (errorsArray.length > 0) {
