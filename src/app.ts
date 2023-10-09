b0f84fef7462f0430788ec90fa596f3dcad9e312
Roman <romkilmetov2000@gmail.com>
2023-10-09 11:37:20 +0400

    comit 45

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { blogsRouter } from "./routes/blogs-router";
 import { postsRouter } from "./routes/posts-router";
 import { testRouter } from "./routes/testing-router";
 import { userRouter } from "./routes/users-router";
+import { authRouter } from "./routes/users-router";
 
 export const app = express()
 
@@ -10,6 +11,7 @@ export const RouterPaths = {
     blogs: '/blogs',
     posts: '/posts',
     users: '/users',
+    auth: '/auth',
     testing: '/testing/all-data'
 }
 
@@ -18,4 +20,5 @@ app.use(parseMiddleware)
 app.use(RouterPaths.blogs, blogsRouter)
 app.use(RouterPaths.posts, postsRouter)
 app.use(RouterPaths.users, userRouter)
+app.use(RouterPaths.auth, authRouter)
 app.use(RouterPaths.testing, testRouter)
